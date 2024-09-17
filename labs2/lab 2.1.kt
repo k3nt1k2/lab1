1d73eb17f1e635e55ca13cb1f7dd84b5b6b142a4
Xamareinc <98962801+k3nt1k2@users.noreply.github.com>
2024-09-18 00:37:47 +0900

    Create lab 2.1.kt

---
+++ labs2/lab 2.1.kt
@@ -0,0 +1,18 @@
+fun main() {
+    print("Введите двузначное число: ")
+    val num = readLine()!!.toInt()
+    val a_num = num / 10
+    val b_num = num % 10
+    val sum_num = a_num + b_num
+    val pro_num = a_num * b_num
+    if(num > 99) {
+        println("ошибка")
+    }
+    else {
+        println("Число десятков: $a_num")
+        println("Число единиц: $b_num")
+        println("Сумма цифр: $sum_num")
+        println("Произведение цифр: $pro_num")
+    }
+
+}
