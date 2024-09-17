4ec9e79e8edaa2e92d1e8fb59d1165968396587e
Xamareinc <98962801+k3nt1k2@users.noreply.github.com>
2024-09-18 00:48:07 +0900

    lab2.3.kt

---
+++ labs2/lab2.3.kt
@@ -0,0 +1,8 @@
+fun main() {
+    print("Первое число: ")
+    val a = readLine()!!.toInt()
+    print("Второе число: ")
+    val b = readLine()!!.toInt()
+    val c = a/b
+    println("Результат: $c")
+}
