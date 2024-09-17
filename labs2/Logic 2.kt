127258b6b0fc95f72632acd17f5d53a0bdeab954
Xamareinc <98962801+k3nt1k2@users.noreply.github.com>
2024-09-18 00:56:41 +0900

    Add files via upload

---
+++ labs2/Logic 2.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val X = false
+    val Y = true
+    val Z = false
+
+    println("а) X или Z: ${(X || Z)}")
+    println("б) X и Y: ${(X && Y)}")
+    println("в) X и Z: ${(X && Z)}")
+}
